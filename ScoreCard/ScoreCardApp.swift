1ff2768dd4213f0b0985c3752550d5d856b972b6
John Parrett <john@johnparrett.com>
2026-02-22 19:31:19 -0500

    Initial Commit

---
+++ ScoreCard/ScoreCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreCardApp.swift
+//  ScoreCard
+//
+//  Created by John Parrett on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct ScoreCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
